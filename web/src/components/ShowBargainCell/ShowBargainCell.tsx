bfc9ea087e255e9925cce01c3b13b2e9b945bbb4
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2023-02-17 13:55:17 +0530

    generate cells for bargains and bargain

---
+++ web/src/components/ShowBargainCell/ShowBargainCell.tsx
@@ -0,0 +1,32 @@
+import type {
+  FindShowBargainQuery,
+  FindShowBargainQueryVariables,
+} from 'types/graphql'
+
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query FindShowBargainQuery($id: Int!) {
+    showBargain: bargain(id: $id) {
+      id
+      product
+      description
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<FindShowBargainQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  showBargain,
+}: CellSuccessProps<FindShowBargainQuery, FindShowBargainQueryVariables>) => {
+  return <div>{JSON.stringify(showBargain)}</div>
+}
---
+++ web/src/components/ShowBargainsCell/ShowBargainsCell.tsx
@@ -0,0 +1,33 @@
+import type { ShowBargainsQuery } from 'types/graphql'
+
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query ShowBargainsQuery {
+    showBargains: bargains {
+      id
+      product
+      description
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  showBargains,
+}: CellSuccessProps<ShowBargainsQuery>) => {
+  return (
+    <ul>
+      {showBargains.map((item) => {
+        return <li key={item.id}>{JSON.stringify(item)}</li>
+      })}
+    </ul>
+  )
+}
