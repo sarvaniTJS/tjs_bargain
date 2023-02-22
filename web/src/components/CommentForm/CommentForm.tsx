e045fce7fd5247598ac25418e5c31a5d2a0e5472
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2023-02-22 09:33:05 +0530

    reset comment form

--- web/src/components/CommentForm/CommentForm.tsx
+++ web/src/components/CommentForm/CommentForm.tsx
@@ -7,6 +7,7 @@ import {
   TextAreaField,
   Submit,
   SubmitHandler,
+  useForm,
 } from '@redwoodjs/forms'
 import { useMutation } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
@@ -33,12 +34,12 @@ interface Props {
 }
 
 const CommentForm = ({ bargainId }: Props) => {
-  const [hasPosted, setHasPosted] = useState(false)
   const { userMetadata } = useAuth()
+  const formMethods = useForm()
 
   const [createComment, { loading, error }] = useMutation(CREATE, {
     onCompleted: () => {
-      setHasPosted(true)
+      formMethods.reset()
       toast.success('Thank you for your comment!')
     },
   })
@@ -60,35 +61,15 @@ const CommentForm = ({ bargainId }: Props) => {
     <>
       <Toaster />
 
-      <div className={hasPosted ? 'hidden' : ''}>
-        <h3 className="font-light text-lg text-gray-600">Leave a Comment</h3>
-        <Form className="mt-4 w-full" onSubmit={onSubmit}>
-          <FormError
-            error={error}
-            titleClassName="font-semibold"
-            wrapperClassName="bg-red-100 text-red-900 text-sm p-3 rounded"
-          />
+      <h3>Leave a Comment</h3>
+      <Form onSubmit={onSubmit} formMethods={formMethods}>
+        <FormError error={error} />
 
-          <Label
-            name="comment"
-            className="block mt-4 text-xs font-semibold text-gray-500 uppercase"
-          >
-            Comment
-          </Label>
-          <TextAreaField
-            name="comment"
-            className="block w-full p-1 border rounded h-24 text-sm"
-            validation={{ required: true }}
-          />
+        <Label name="comment">Comment</Label>
+        <TextAreaField name="comment" validation={{ required: true }} />
 
-          <Submit
-            disabled={loading}
-            className="block mt-4 bg-blue-500 text-white text-xs font-semibold uppercase tracking-wide rounded px-3 py-2 disabled:opacity-50"
-          >
-            Submit
-          </Submit>
-        </Form>
-      </div>
+        <Submit disabled={loading}>Submit</Submit>
+      </Form>
     </>
   )
 }
