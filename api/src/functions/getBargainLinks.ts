e534c784be9fd2eb2c305aa480677fb1e57c2026
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2023-03-03 12:47:26 +0530

    console bargain

--- api/src/functions/getBargainLinks.ts
+++ api/src/functions/getBargainLinks.ts
@@ -7,6 +7,7 @@ exports.handler = async function (event) {
   const bargain = await db.bargain.findUnique({
     where: { id: bargainId },
   }).product
+  console.log('bargain------->', bargain)
   const start = Date.now()
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
