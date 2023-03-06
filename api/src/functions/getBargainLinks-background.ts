f4f9afdda3fcf38f4f2d7ed1054a007b18cf8bb3
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2023-03-06 23:12:54 +0530

    debugging

--- api/src/functions/getBargainLinks-background.ts
+++ api/src/functions/getBargainLinks-background.ts
@@ -34,11 +34,12 @@ exports.handler = async function (event) {
   await page.goto(goto)
   await page.waitForSelector('.UxuaJe.shntl.FkMp')
   const linkList = await page.evaluate(() => {
-    const linkElements = Array.from(document.querySelectorAll('.b5ycib.shntl'))
-    const links = linkElements.map((ele) => {
-      return ele.href
-    })
-    return links
+    return document.querySelector('.b5ycib.shntl').href
+    // const linkElements = Array.from(document.querySelectorAll('.b5ycib.shntl'))
+    // const links = linkElements.map((ele) => {
+    //   return ele.href
+    // })
+    // return links
   })
   console.log('linkList------>', linkList)
   await browser.close()
