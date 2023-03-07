42d6d5e657c2b3714f59a1d03a439d6694a2a7d6
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2023-03-07 15:22:18 +0530

    implement css for bargain page

--- web/src/components/ShowBargainCell/ShowBargainCell.tsx
+++ web/src/components/ShowBargainCell/ShowBargainCell.tsx
@@ -1,3 +1,6 @@
+import { Fragment } from 'react'
+
+import { CalendarIcon } from '@heroicons/react/20/solid'
 import type {
   FindShowBargainQuery,
   FindShowBargainQueryVariables,
@@ -11,6 +14,10 @@ import Comment from '../Comment/Comment'
 import CommentForm from '../CommentForm/CommentForm'
 import VoteForm from '../VoteForm/VoteForm'
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 export const QUERY = gql`
   query FindShowBargainQuery($id: Int!) {
     showBargain: bargain(id: $id) {
@@ -19,6 +26,10 @@ export const QUERY = gql`
       description
       upvoteCount
       downvoteCount
+      user {
+        userName
+        createdAt
+      }
       comments {
         id
         comment
@@ -56,8 +67,33 @@ export const Success = ({
 
   return (
     <div>
-      <h2>{showBargain.product}</h2>
-      <p>{showBargain.description}</p>
+      <div className="lg:flex lg:items-center lg:justify-between">
+        <img
+          className="h-12 w-12 rounded-full mr-3"
+          src="https://s.gravatar.com/avatar/422d7242d6a1a55e64842f4be6c05c0b?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fus.png"
+          alt=""
+        />
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+            {showBargain.product}
+          </h2>
+          <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+            <div className="mt-1 flex items-center text-sm text-gray-500">
+              by {showBargain.user.userName}
+            </div>
+            <div className="mt-1 flex items-center text-sm text-gray-500">
+              <CalendarIcon
+                className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
+              />
+              {showBargain.user.createdAt}
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-l text-gray-700 leading-8">
+        {showBargain.description}
+      </p>
       {isAuthenticated && <VoteForm bargainId={showBargain.id} />}
       <p>Upvotes: {showBargain.upvoteCount}</p>
       <p>Downvotes: {showBargain.downvoteCount}</p>
