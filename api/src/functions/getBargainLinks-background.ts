7954fae8b4840fa800d312f32806949b4771930d
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2023-03-10 19:18:02 +0530

    use other selector

--- api/src/functions/getBargainLinks-background.ts
+++ api/src/functions/getBargainLinks-background.ts
@@ -35,67 +35,68 @@ exports.handler = async function (event) {
     return document.querySelector('div.f0t7kf').textContent
   })
   console.log('modelName', modelName)
-  let data = await page.evaluate(() => {
-    let rows = Array.from(document.querySelectorAll('tr.sh-osd__offer-row'))
-    rows = rows.map((r) => {
-      let source = r.querySelector('td > div > a')?.text
-      if (source) {
-        source = source.split('Opens')[0]
-      }
-      let link = r.querySelector('td:nth-child(5) > div > a')?.href
-      if (link) {
-        link = link.split('q=')[1].split('%')[0].split('&')[0]
-      }
-      const price = r.querySelector('td:nth-child(4) > div > div')?.innerText
-      return { source, link, price }
-    })
-    return rows
-  })
-
-  data = data.filter((d) => d.link.includes('http'))
+  const data = await page.$$('tr.sh-osd__offer-row')
   console.log('data', data)
-  await browser.close()
+  // let data = await page.evaluate(() => {
+  //   let rows = Array.from(document.querySelectorAll('tr.sh-osd__offer-row'))
+  //   rows = rows.map((r) => {
+  //     let source = r.querySelector('td > div > a')?.text
+  //     if (source) {
+  //       source = source.split('Opens')[0]
+  //     }
+  //     let link = r.querySelector('td:nth-child(5) > div > a')?.href
+  //     if (link) {
+  //       link = link.split('q=')[1].split('%')[0].split('&')[0]
+  //     }
+  //     const price = r.querySelector('td:nth-child(4) > div > div')?.innerText
+  //     return { source, link, price }
+  //   })
+  //   return rows
+  // })
 
-  const end = Date.now()
-  const timeTaken = end - start
-  console.log(`seconds elapsed = ${Math.floor(timeTaken / 1000)}`)
-  try {
-    await db.$transaction(async (tx) => {
-      const existingLink = await tx.link.findFirst({
-        where: {
-          bargainId,
-        },
-      })
-      console.log('existingLink====>', existingLink)
-      if (existingLink !== null) {
-        const delLink = await tx.link.deleteMany({
-          where: {
-            bargainId,
-          },
-        })
-        console.log('delLink=======>', delLink)
-      }
+  // data = data.filter((d) => d.link.includes('http'))
+  // await browser.close()
 
-      data.forEach(async (d) => {
-        await tx.link.create({
-          data: {
-            bargainId,
-            link: d.link,
-            source: d.source,
-            price: d.price,
-            model: modelName,
-          },
-        })
-      })
-      return {
-        statusCode: 200,
-      }
-    })
-    console.log('new data created')
-  } catch (error) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: error.message }),
-    }
-  }
+  // const end = Date.now()
+  // const timeTaken = end - start
+  // console.log(`seconds elapsed = ${Math.floor(timeTaken / 1000)}`)
+  // try {
+  //   await db.$transaction(async (tx) => {
+  //     const existingLink = await tx.link.findFirst({
+  //       where: {
+  //         bargainId,
+  //       },
+  //     })
+  //     console.log('existingLink====>', existingLink)
+  //     if (existingLink !== null) {
+  //       const delLink = await tx.link.deleteMany({
+  //         where: {
+  //           bargainId,
+  //         },
+  //       })
+  //       console.log('delLink=======>', delLink)
+  //     }
+
+  //     data.forEach(async (d) => {
+  //       await tx.link.create({
+  //         data: {
+  //           bargainId,
+  //           link: d.link,
+  //           source: d.source,
+  //           price: d.price,
+  //           model: modelName,
+  //         },
+  //       })
+  //     })
+  //     return {
+  //       statusCode: 200,
+  //     }
+  //   })
+  //   console.log('new data created')
+  // } catch (error) {
+  //   return {
+  //     statusCode: 400,
+  //     body: JSON.stringify({ error: error.message }),
+  //   }
+  // }
 }
